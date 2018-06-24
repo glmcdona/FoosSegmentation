5bce401b8d78d45064a626220c8aac9d4dde3e36
Geoff McDonald <glmcdona@gmail.com>
2018-06-23 22:28:11 -0700

    Add next-frame prediction model code and line drawing utilities for preparing rod tracking data.

---
+++ Data/process_add_rod_lines.cmd
@@ -0,0 +1,1 @@
+python ..\Code\process.py process add_rod_lines.json
---
+++ Data/process_next_frame_prediction.cmd
@@ -0,0 +1,2 @@
+python ..\Code\process.py process nextframe_train.json
+python ..\Code\process.py process nextframe_validate.json
